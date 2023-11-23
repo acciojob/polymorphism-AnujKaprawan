13d5a19dad626b4bec34cb9eba2cfde1f15d1e32
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2023-11-23 18:02:10 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,17 +17,8 @@ public class Main {
     public static void main(String[] args) {
         // Step 2: Create an object of Product in Main function called p
         Product p = new Product();
-
-        // Step 3: Call the method public int product(int x, int y) from Main using Product class object p
-        int result1 = p.product(2, 3);
-        System.out.println("Product of 2 and 3: " + result1); // Prints: Product of 2 and 3: 6
-
-        // Step 4: Call the method public int product(int x, int y, int z) from Main using Product class object p
-        int result2 = p.product(2, 3, 4);
-        System.out.println("Product of 2, 3, and 4: " + result2); // Prints: Product of 2, 3, and 4: 24
-
-        // Step 5: Call the method public double product(double x, double y) from Main using Product class object p
-        double result3 = p.product(2.5, 3.5);
-        System.out.println("Product of 2.5 and 3.5: " + result3); // Prints: Product of 2.5 and 3.5: 8.75
+         p.product(2, 3);
+         p.product(2, 3, 4);
+         p.product(2.5, 3.5);
     }
 }
